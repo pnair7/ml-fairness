7156c6f005ece171c922dbf70e71ad1c9d94cfd7
Praveen Nair <prnair@ucsd.edu>
2022-01-06 21:12:12 -0800

    Write some main pseudocode

---
+++ main.py
@@ -0,0 +1,21 @@
+
+## import libraries
+
+## define lists of models, datasets, and metrics
+
+## iterate through (model, dataset, metric) tuples
+
+#for model in models:
+#	for dataset in datasets:
+#		## read in data (should already be cleaned and in standard format)
+#		
+#		## define model w/ parameters if needed
+#
+#		## apply model to dataset, yield result with predictions
+#
+#		## import functions for each fairness metric on standardized output format
+#		for metric in metrics:
+#			## apply fairness metric to predictions
+#			
+#			## output result (probably by appending to list that becomes result matrix
+
