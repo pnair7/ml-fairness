6497edb7fc76813602290c7dcef68e59279680a5
Praveen Nair <prnair@ucsd.edu>
2022-01-25 13:39:42 -0800

    Make models return results as lists

--- models/sklearn_models.py
+++ models/sklearn_models.py
@@ -4,11 +4,11 @@ from sklearn.tree import DecisionTreeClassifier
 def logistic_regression(X_train, X_test, y_train, y_test, group_train, group_test, config, class_weight = 'balanced'):
     mdl = LogisticRegression(class_weight = class_weight, solver = 'sag', max_iter = 1000, random_state = 42)
     mdl.fit(X_train, y_train)
-    results = mdl.predict(X_test)
+    results = list(mdl.predict(X_test))
     return results, mdl
 
 def decision_tree(X_train, X_test, y_train, y_test, group_train, group_test, config):
     clf = DecisionTreeClassifier(random_state = 42)
     clf.fit(X_train, y_train)
-    results = clf.predict(X_test)
+    results = list(clf.predict(X_test))
     return results, clf
